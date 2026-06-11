675edc22fcb8520d622fbfdc86f5b006f8db8125
Bolivar Y. Vergara III <bolivar1985@yahoo.com>
2026-06-11 17:40:02 +0800

    Fix file paths for vercel backend

--- api/index.php
+++ api/index.php
@@ -1,3 +1,19 @@
 <?php
-// Forward execution straight to Laravel's public handler
-require __DIR__ . '/../public/index.php';
+
+// 1. Register the Composer Autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Boot the Laravel Framework Application
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// 3. Handle the Incoming Web Request using Laravel's Kernel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+// 4. Send the Output Response back to the user's browser
+$response->send();
+
+$kernel->terminate($request, $response);
